9fa0391ebd41151520246119944506be8190ad55
Diego Campos <diegodiko05@gmail.com>
2020-06-11 20:46:07 -0300

    Montando as rotas para cada filme

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,19 +1,38 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
-  render() {
+  constructor(props) {
+    super(props);
     const { title, imagePath, storyline, id } = this.props.movie;
+    this.state = {
+      title: title,
+      imagePath: imagePath,
+      storyline: storyline,
+      id: id,
+    };
+  }
+
+  render() {
     return (
       <div data-testid="movie-card">
-        <img src={imagePath} alt={title}/>
-        <h4>{title}</h4>
-        <p>{storyline}</p>
-        <Link to={`movies/:${id}`}>VER DETALHES</Link>
+        <img src={this.state.imagePath} alt={this.state.title} />
+        <h4>{this.state.title}</h4>
+        <p>{this.state.storyline}</p>
+        <Link to={`movies/${this.state.id}`}>VER DETALHES</Link>
         {/* {console.log(movie)} */}
       </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    id: PropTypes.number.isRequired,
+    storyline: PropTypes.string.isRequired,
+  }).isRequired,
+};
 
 export default MovieCard;
--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -4,15 +4,31 @@ import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
 
 class MovieDetails extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: false,
+      movie: null,
+    };
+  }
+
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    movieAPI.getMovie(id).then((movie) => this.setState({ movie, isLoading: true }));
+    console.log(id);
+  }
+
   render() {
     // Change the condition to check the state
-    if (true) return <Loading />;
+    const { isLoading, movie } = this.state;
+    if (!isLoading) return <Loading />;
 
     const { title, storyline, imagePath, genre, rating, subtitle } = movie;
 
     return (
       <div data-testid="movie-details">
         <img alt="Movie Cover" src={`../${imagePath}`} />
+        <h4>{title}</h4>
         <p>{`Subtitle: ${subtitle}`}</p>
         <p>{`Storyline: ${storyline}`}</p>
         <p>{`Genre: ${genre}`}</p>
