079c837017537f0b2eb1ffd411c7772e7352cf78
Daniel Pantalena <pantalenadaniel@gmail.com>
2020-06-11 15:05:58 -0300

    cc2

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,8 +1,8 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
-import PropTypes from 'prop-types';
 
 class MovieDetails extends Component {
   constructor(props) {
