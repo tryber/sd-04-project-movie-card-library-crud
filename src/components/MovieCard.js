75a26f61fb81370b6ba95cbad213deb4e459b8de
cacobribeiro <cacobribeiro@gmail.com>
2020-06-11 17:54:56 -0300

    Test PropTypes

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -2,6 +2,7 @@ import PropTypes from 'prop-types';
 import React from 'react';
 // import movies from '../services/movieData';
 import '../style.css';
+import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
@@ -15,6 +16,9 @@ class MovieCard extends React.Component {
             <p className="movie-card-subtitle">{subtitle}</p>
             <p className="movie-card-storyline">{storyline}</p>
             <p className="rating">{rating}</p>
+            <Link>
+              <small>Ver Detalhes</small>{' '}
+            </Link>
           </div>
         </div>
       </div>
