27acc605bfd8bb5c7aacdd32909da525a5ed9cfc
Caio Magalhães <caio.lmf@gmail.com>
2020-06-12 14:58:39 -0300

    Set MovieLibrary  as root route and render on app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import * as ROUTES from '../constants/routes';
+import { MovieLibrary } from './index';
+
+import Index from './components/index';
 
 function App() {
   return (
+    <BrowserRouter>
       <div>Movie Card Library CRUD</div>
+      <Route to={ROUTES.ROOT} component={MovieLibrary}></Route>
+    </BrowserRouter>
   );
 }
 
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Loading } from './Loading';
+export { default as MovieLibrary } from './MovieLibrary';
 export { default as MovieForm } from './MovieForm';
 export { default as MovieCard } from './MovieCard';
