ab3b8cef470b90792e03c4bb7c5ea58ec0ca4947
Diego Campos <diegodiko05@gmail.com>
2020-06-11 22:07:30 -0300

    CC problems

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 
 import MovieList from './pages/MovieList';
 import MovieDetails from './pages/MovieDetails';
-import EditMovie from './pages/EditMovie'
+import EditMovie from './pages/EditMovie';
 
 function App() {
   return (
@@ -12,7 +12,6 @@ function App() {
       <Route exact path="/" component={MovieList} />
       <Route path="/movies/:id" component={MovieDetails} />
       <Route path="/movies/:id/Edit" component={EditMovie} />
-
     </Router>
   );
 }
--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -31,7 +31,7 @@ MovieCard.propTypes = {
     title: PropTypes.string.isRequired,
     id: PropTypes.number.isRequired,
     storyline: PropTypes.string.isRequired,
-    imagePath: PropTypes.string.isRequired
+    imagePath: PropTypes.string.isRequired,
   }).isRequired,
 };
 
