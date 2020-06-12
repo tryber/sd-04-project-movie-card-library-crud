33b17ba1e725560bc6cb5ef07aef493b173f74e9
Rangel Paolo <paolorangel1996@outlook.com>
2020-06-12 18:14:36 -0300

    Adicionadas 4 rotas de navegação da página.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import { MovieList } from './pages';
 
 function App() {
   return (
+    <div>
       <div>Movie Card Library CRUD</div>
+      <Switch>
+        <Route exact path="/" component={MovieList} />
+        <Route path="/movies/new" component={MovieList} />
+        <Route path="/movies/:id/edit" component={MovieList} />
+        <Route path="/movies/:id" component={MovieList} />
+      </Switch>
+    </div>
   );
 }
 
