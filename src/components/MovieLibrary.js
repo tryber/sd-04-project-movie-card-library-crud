b0b5520d279f53740c2125084caf4e5ad2ec0521
Caio Magalhães <caio.lmf@gmail.com>
2020-06-12 14:56:27 -0300

    Create MovieLibrary component

---
+++ src/components/MovieLibrary.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MovieLibrary extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Movie Library</h1>
+      </div>
+    );
+  }
+}
+
+export default MovieLibrary;
