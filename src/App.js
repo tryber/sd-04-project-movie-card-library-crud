30fe2fb41ff48df8c6490eb172f755c975ecf6bd
Caio Magalhães <caio.lmf@gmail.com>
2020-06-12 15:06:38 -0300

    Do some corrections on routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
-import * as ROUTES from '../constants/routes';
-import { MovieLibrary } from './index';
+import * as ROUTES from './constants/routes';
+import MovieLibrary from './components/MovieLibrary';
 
 import Index from './components/index';
 
