4146679258f9e67a02f9cbc33e2d3e76236dc023
Caio Magalhães <caio.lmf@gmail.com>
2020-06-12 18:27:18 -0300

    Add loading component and implement on movielist api call

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,10 +1,34 @@
 import React, { Component } from 'react';
+import { getMovies } from '../services/movieAPI';
+
+import Loading from './Loading';
 
 class MovieList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoading: true,
+      movies: [],
+    };
+  }
+
+  componentDidMount = () => {
+    getMovies().then((data) => {
+      this.setState({
+        isLoading: false,
+        movies: data,
+      });
+    });
+  };
+
   render() {
+    const { isLoading } = this.state;
     return (
       <div>
         <h1>Movie Library</h1>
+        {isLoading ? <Loading /> : null}
+        <div data-testid="movie-list"></div>
       </div>
     );
   }
