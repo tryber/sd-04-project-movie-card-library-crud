905fc315f1056d9bc8b50c98261098c04478e5fb
Lourenço <loulage@gmail.com>
2020-06-12 17:18:34 -0300

    Added function to fetch mocked API on MovieList

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
-
+import Loading from '../components/Loading';
 import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+      movies: '',
+    }
+  }
+  
+  componentDidMount() {
+    this.setState({ movies: movieAPI.getMovies() });
+  }
+
   render() {
     const { movies } = this.state;
 
-    // Render Loading here if the request is still happening
-
+    if (movies === '') return <Loading />
+    
     return (
       <div data-testid="movie-list">
         {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
