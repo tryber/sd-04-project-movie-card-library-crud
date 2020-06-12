1e301f3afcd5db8519dc03a972816108f8798403
juniomelos <juniomelos@gmail.com>
2020-06-11 22:03:45 -0300

    Cypress stuck - by testing, done - 1 - Route check. 2 - Movie list component. 3 - Movie card component ... - CC issues - 4

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -29,7 +29,7 @@ MovieCard.propTypes = {
     title: PropTypes.string.isRequired,
     id: PropTypes.number.isRequired,
     storyline: PropTypes.string.isRequired,
-    ImagePath: PropTypes.string.isRequired,
+    imagePath: PropTypes.string.isRequired,
   }).isRequired,
 };
 
