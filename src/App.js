217779211cd8a406960b1793a5660651697d34ec
Rangel Paolo <paolorangel1996@outlook.com>
2020-06-13 01:00:07 -0300

    Trocado BrowserRouter de lugar.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import { MovieList, NewMovie, EditMovie, MovieDetails } from './pages';
 
 function App() {
   return (
-    <div>
-      <div>Movie Card Library CRUD</div>
-      <Switch>
-        <Route exact path="/" component={MovieList} />
-        <Route path="/movies/new" component={NewMovie} />
-        <Route path="/movies/:id/edit" component={EditMovie} />
-        <Route path="/movies/:id" component={MovieDetails} />
-      </Switch>
-    </div>
+    <BrowserRouter>
+      <div>
+        <div>Movie Card Library CRUD</div>
+        <Switch>
+          <Route exact path="/" component={MovieList} />
+          <Route path="/movies/new" component={NewMovie} />
+          <Route path="/movies/:id/edit" component={EditMovie} />
+          <Route path="/movies/:id" component={MovieDetails} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
