bc330b1f8efa865474d582f246122bc94a803a72
orlando-messias <programadorlando@gmail.com>
2020-06-13 13:44:52 -0300

    MovieDetails

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -4,15 +4,30 @@ import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
 
 class MovieDetails extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      isLoaded: false,
+      movie: {},
+    };
+  }
+
+  componentDidMount() {
+    const id = this.props.match.params.id;
+    movieAPI.getMovie(id)
+      .then(result => this.setState({ movie: result, isLoaded: true }));
+  }
+
   render() {
     // Change the condition to check the state
-    if (true) return <Loading />;
+    if (!this.state.isLoaded) return <Loading />;
 
-    const { title, storyline, imagePath, genre, rating, subtitle } = movie;
+    const { title, storyline, imagePath, genre, rating, subtitle } = this.state.movie;
 
     return (
       <div data-testid="movie-details">
         <img alt="Movie Cover" src={`../${imagePath}`} />
+        <p>{`Title: ${title}`}</p>
         <p>{`Subtitle: ${subtitle}`}</p>
         <p>{`Storyline: ${storyline}`}</p>
         <p>{`Genre: ${genre}`}</p>
