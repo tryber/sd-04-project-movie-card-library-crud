cd379389d77d98386159c63c3276d90ea5b4b781
orlando-messias <programadorlando@gmail.com>
2020-06-13 13:00:48 -0300

    router settings App.js + MovieList

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -4,16 +4,34 @@ import MovieCard from '../components/MovieCard';
 import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoaded: false,
+      movies: [],
+    };
+  }
+
+  componentDidMount() {
+    movieAPI.getMovies()
+      .then(data => {
+        this.setState({ isLoaded: true, movies: data });
+      });
+  }
+
   render() {
-    const { movies } = this.state;
+    const { isLoaded, movies } = this.state;
 
-    // Render Loading here if the request is still happening
-
-    return (
-      <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
-      </div>
-    );
+    if (!isLoaded) return <div>Carregando...</div>
+    else {
+      return (
+        <div data-testid="movie-list">
+          <div className="main">
+            {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
+          </div>
+        </div>
+      );
+    }
   }
 }
 
