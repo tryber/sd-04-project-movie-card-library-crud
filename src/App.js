61f3c0feecdfe992e4926596d04a18cf46cc9377
Thiago Tavares <thiagooo15@gmail.com>
2020-06-13 13:31:18 -0300

    primeiro cc

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
 
 function App() {
-	return (
-		<Router>
-			<div>Movie Card Library CRUD</div>
-		</Router>
-	);
+  return (
+    <Router>
+      <div>Movie Card Library CRUD</div>
+    </Router>
+  );
 }
 
 export default App;
