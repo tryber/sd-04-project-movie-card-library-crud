ffb64e9f18c066422354cec840912de3ebdcb88f
Rangel Paolo <paolorangel1996@outlook.com>
2020-06-12 21:32:14 -0300

    Implementada página de edição do filme.

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,26 +1,38 @@
 import React, { Component } from 'react';
-
-import { MovieForm } from '../components';
+import { Redirect } from 'react-router-dom';
+import { MovieForm, Loading } from '../components';
+import { getMovie } from '../services/movieAPI';
 import * as movieAPI from '../services/movieAPI';
 
 class EditMovie extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      status: 'loading',
+      shouldRedirect: null,
+      movie: null,
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  componentDidMount() {
+    getMovie(this.props.match.params.id)
+      .then((movie) => this.setState({ movie, status: null }));
+  }
+
   handleSubmit(updatedMovie) {
+    this.setState({ shouldRedirect: true });
   }
 
   render() {
     const { status, shouldRedirect, movie } = this.state;
+
     if (shouldRedirect) {
-      // Redirect
+      return <Redirect to="/" />
     }
 
     if (status === 'loading') {
-      // render Loading
+      return <Loading />
     }
 
     return (
