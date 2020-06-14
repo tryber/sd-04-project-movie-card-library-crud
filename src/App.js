fbde07959a4be5c72c0dba843dd4f6537ee55f88
matheusysd <matheusyurisd@gmail.com>
2020-06-14 03:45:18 -0300

    CC 5

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import MovieList from './pages/MovieList';
 import NewMovie from './pages/NewMovie';
 import MovieDetails from './pages/MovieDetails';
@@ -8,8 +8,6 @@ import EditMovie from './pages/EditMovie';
 function App() {
   return (
     <Router>
-      {/* <div>Movie Card Library CRUD</div>
-      <MovieList /> */}
       <Link to="/movies/new">ADICIONAR CARTÃO</Link>
       <Switch>
         <Route exact path="/movies/new" component={NewMovie} />
