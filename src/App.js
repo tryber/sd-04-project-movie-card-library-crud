a70a9585fe3d4ede6fc62455b627f10598b8d564
matheusysd <matheusyurisd@gmail.com>
2020-06-14 01:44:25 -0300

    Requisito 1 e 2 ok

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import MovieList from './pages/MovieList';
 
 function App() {
   return (
     <Router>
       <div>Movie Card Library CRUD</div>
+      <MovieList />
     </Router>
   );
 }
--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
+import Loading from '../components/Loading';
 
 import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      movies: '',
+    };
+  }
+  componentDidMount(){
+    movieAPI.getMovies()
+    .then(res => this.setState({movies: res}))
+  }
   render() {
     const { movies } = this.state;
-
+    
     // Render Loading here if the request is still happening
 
-    return (
+
+    return movies === '' ? <Loading /> :
+    (
       <div data-testid="movie-list">
+        {console.log(this.state.movies)}
         {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
       </div>
     );
