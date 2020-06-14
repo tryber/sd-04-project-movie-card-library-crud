f91a155d97e0f92f2bbfb861eccf6ca7ec83e368
Tiago-ers <trsilva284@gmail.com>
2020-06-14 08:23:49 -0300

    cc - correçao index

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
 import { Switch, Route } from 'react-router-dom';
 import { MovieDetails, MovieList, NewMovie, EditMovie, NotFound } from './pages';
 import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div>Movie Card Library CRUD</div>
-      <Switch>
-        <Route path="/movies/:id" component={MovieDetails} />
-        <Route path="/movies/new" component={NewMovie} />
-        <Route path="/movies/:id/edit" component={EditMovie} />
-        <Route exact path="/" component={MovieList} />
-        <Route component={NotFound} />
-      </Switch>
-    </Router>
+  <div>
+    Movie Card Library CRUD
+    <Switch>
+      <Route path="/movies/:id" component={MovieDetails} />
+      <Route path="/movies/new" component={NewMovie} />
+      <Route path="/movies/:id/edit" component={EditMovie} />
+      <Route exact path="/" component={MovieList} />
+      <Route component={NotFound} />
+    </Switch>
+  </div>
   );
 }
 
