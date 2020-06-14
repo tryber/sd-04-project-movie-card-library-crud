41fb67dbe401a81e824843af2ebf19d9b081470e
Tiago-ers <trsilva284@gmail.com>
2020-06-14 07:30:46 -0300

    iniciando o projeto. VAMOS COM TUDO :rocket:

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route} from 'react-router-dom';
+import { MovieDetails, MovieList, NewMovie, EditMovie, NotFound } from './pages';
+import './App.css';
 
 function App() {
   return (
+    <BrowserRouter>
       <div>Movie Card Library CRUD</div>
+      <Switch>
+        <Route path="/movies/:id" component={MovieDetails} />
+        <Route path="/movies/new" component={NewMovie} />
+        <Route path="/movies/:id/edit" component={EditMovie} />
+        <Route exact path="/" component={MovieList} />
+        <Route component={NotFound} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -4,12 +4,18 @@ import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
 
 class MovieDetails extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movie: "",
+    }
+  }
   render() {
     // Change the condition to check the state
     if (true) return <Loading />;
 
     const { title, storyline, imagePath, genre, rating, subtitle } = movie;
-
+    const { movie } = this.state
     return (
       <div data-testid="movie-details">
         <img alt="Movie Cover" src={`../${imagePath}`} />
