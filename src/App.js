8c104af4533869cf7c373a9f1cccc9ca28c98e5c
Lucas Ribas <lucas@mundoribas.com.br>
2020-06-15 18:18:30 -0300

    Requisito 1 - ajustes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
+import { MovieList, NewMovie, MovieDetails, EditMovie, NotFound } from './pages'
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <Switch>
         <div>Movie Card Library CRUD</div>
-        <Route exact path='/' component={MovieList} />
-        <Route exact path='/movies/new' component={NewMovie} />
-        <Route exact path='/movies/:id' component={MovieDetails} />
-        <Route exact path='/movies/:id/edit' component={EditMovie} />
-        <Route exact path='/' component={NotFound} />
+        <Route exact path="/" component={MovieList} />
+        <Route exact path="/movies/new" component={NewMovie} />
+        <Route exact path="/movies/:id" component={MovieDetails} />
+        <Route exact path="/movies/:id/edit" component={EditMovie} />
+        <Route exact path="/" component={NotFound} />
       </Switch>
-    </Router>
+    </BrowserRouter>
   );
 }
 
