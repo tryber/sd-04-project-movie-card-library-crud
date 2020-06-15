5f410a8810e7d4bb6888b5577c21b8caf5025ebc
deboracosilveira <deboracosilveira@gmail.com>
2020-06-15 16:39:36 -0300

    requirement-1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { MovieList, MovieDetails, NewMovie, EditMovie, NotFound } from './pages/index';
 
 class App extends React.Component {
   render() {
-    return <div>Movie Card Library CRUD</div>;
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={MovieList}/>
+          <Route exact path="/movies/:id" component={MovieDetails}/>
+          <Route exact path="/movies/new" component={NewMovie}/>
+          <Route exact path="/movies/:id/edit" component={EditMovie}/>
+          <Route exact path="/" component={NotFound}/>
+        </Switch>
+      </BrowserRouter>
+    )
   }
 }
 
