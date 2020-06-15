521e94fcd75fac46f887030cc067ba10f90adffb
Lucas Ribas <lucas@mundoribas.com.br>
2020-06-15 18:21:48 -0300

    Faltou um ponto e vírgula

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
-import { MovieList, NewMovie, MovieDetails, EditMovie, NotFound } from './pages'
+import { MovieList, NewMovie, MovieDetails, EditMovie, NotFound } from './pages';
 
 function App() {
   return (
