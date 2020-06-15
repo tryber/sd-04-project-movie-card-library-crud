aeab93d1b4831e59262b54f9d12d330fe6c5897b
douglaza <douglaza@gmail.com>
2020-06-15 17:59:52 -0300

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
+import './App.css';
+import { BrowserRouter, Switch, Route, Link, Redirect } from 'react-router-dom';
+import MovieList from './pages/MovieList';
+import NewMovie from './pages/NewMovie';
+// import MovieDetails from './pages/MovieDetails';
+import EditMovie from './pages/EditMovie';
 
-function App() {
-  return (
-      <div>Movie Card Library CRUD</div>
-  );
+// function App() {
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <header>
+          <ul>
+            <li>
+              <Link to="/">Lista de Filmes</Link>
+            </li>
+            <li>
+              <Link to="/movies/:id">Novo Filme</Link>
+            </li>
+            <li>
+              <Link to="/movies/new">Detalhes</Link>
+            </li>
+            <li>
+              <Link to="/movies/:id/edit">Editar Filme</Link>
+            </li>
+          </ul>
+        </header>
+        <main>
+          <Switch>
+            {/* <Route exact path="/" component={MovieList} /> */}
+            <Route exact path="/movies/:id" component={NewMovie} />
+            {/* <Route exact path="/movies/new" component={MovieDetails} /> */}
+            <Route exact path="/movies/:id/edit" component={EditMovie} />
+          </Switch>
+        </main>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
+
+// {/* <div>Movie Card Library CRUD</div> */}
