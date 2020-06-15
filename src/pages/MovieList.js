baccb11d0e7980fd97cdc780850d31dc833f4ba4
frederico-campello <fredfmc33@gmail.com>
2020-06-15 18:21:01 -0300

    Componente MovieList

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -2,16 +2,39 @@ import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
 
 import * as movieAPI from '../services/movieAPI';
+import Loading from '../components/Loading';
 
 class MovieList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: [],
+      loading: true
+    };
+  }
+
+  componentDidMount() {
+    // função getMovies chamada assim que o componente é montado
+
+    movieAPI.getMovies().then((movies) => {
+      this.setState({ 
+      movies: movies,
+      loading: false
+    });
+      // console.log(this.state.movies)
+    });
+  }
+
   render() {
-    const { movies } = this.state;
-
-    // Render Loading here if the request is still happening
-
+    const { movies, loading } = this.state;
+    if (loading === true) {
+      return <Loading />;
+    }
     return (
       <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
+        {movies.map((movie) => (
+          <MovieCard key={movie.title} movie={movie} />
+        ))}
       </div>
     );
   }
