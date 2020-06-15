83571f730a35f4c5e53972ad871e226332720de2
Fabiola Deiro <fabioladeiro7@gmail.com>
2020-06-15 17:49:21 -0300

    Requisitos 3 e 4

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
+import EditMovie from './pages/EditMovie';
 
 function App() {
   return (
     <BrowserRouter>
-      <div>Movie Card Library CRUD</div>
-      <MovieList />
+      <Switch>
+        <Route exact path="/" component={MovieList} />
+        <Route exact path="/movies/:id" component={MovieDetails} />
+        <Route exact path="/movies/:id/edit" component={EditMovie} />
+      </Switch>
     </BrowserRouter>
   );
 }
--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
-    const { title, subtitle, storyline, imagePath } = movie;
+    const { title, id, subtitle, storyline, imagePath } = movie;
     return (
       <div data-testid="movie-card">
-        <img alt="Imagem do Filme" src={imagePath} />
+        <img alt="Movie Cover" src={imagePath} />
         <div>
           <h2>{title}</h2>
           <h3>{subtitle}</h3>
           <p>{storyline}</p>
         </div>
+        <Link to={`/movies/${id}`}>VER DETALHES</Link>
       </div>
     );
   }
@@ -21,6 +23,7 @@ class MovieCard extends React.Component {
 MovieCard.propTypes = {
   movie: PropTypes.shape({
     title: PropTypes.string.isRequired,
+    id: PropTypes.number.isRequired,
     subtitle: PropTypes.string.isRequired,
     storyline: PropTypes.string.isRequired,
     imagePath: PropTypes.string.isRequired,
