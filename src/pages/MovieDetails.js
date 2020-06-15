de9f594944747c39d5f7441f5db857915b4d1092
Lourenço <loulage@gmail.com>
2020-06-15 19:11:15 -0300

    ccfix

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -33,7 +33,7 @@ class MovieDetails extends Component {
       <div data-testid="movie-details">
         <img alt="Movie Cover" src={`../${imagePath}`} />
         <p>{`Title: ${title}`}</p>
-        <p>{`Subtitle: ${subtitle}`}</p>
+        <p>{`Subtitle:  ${subtitle}`}</p>
         <p>{`Storyline: ${storyline}`}</p>
         <p>{`Genre: ${genre}`}</p>
         <p>{`Rating: ${rating}`}</p>
