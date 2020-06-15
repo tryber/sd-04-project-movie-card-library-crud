204ccc372949da2178a0c160d9fdde646010b529
AliceAAtalla <aliceatalla36@gmail.com>
2020-06-15 19:52:02 -0300

    EditMovie and Routes Update

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,26 +1,40 @@
-import React, { Component } from 'react';
-
-import { MovieForm } from '../components';
-import * as movieAPI from '../services/movieAPI';
+import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
+import { MovieForm, Loading } from "../components";
+import * as movieAPI from "../services/movieAPI";
 
 class EditMovie extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      status: "loading",
+      shouldRedirect: false,
+      movie: null,
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  componentDidMount() {
+    movieAPI
+      .getMovie(this.props.match.params.id)
+      .then((movie) => this.setState({ status: "finish loading", movie }));
+  }
+
   handleSubmit(updatedMovie) {
+    movieAPI.updateMovie(updatedMovie).then(() => {
+      this.setState({ shouldRedirect: true });
+    });
   }
 
   render() {
     const { status, shouldRedirect, movie } = this.state;
+
     if (shouldRedirect) {
-      // Redirect
+      return <Redirect to="/" />;
     }
 
-    if (status === 'loading') {
-      // render Loading
+    if (status === "loading") {
+      return <Loading />;
     }
 
     return (
