54c439cd1c19c7a6e0062181f8ecd8293a0a9f15
Lucas Ribas <lucas@mundoribas.com.br>
2020-06-15 17:54:56 -0300

    Primeiro requisito

--- src/App.js
+++ src/App.js
@@ -5,7 +5,14 @@ import './App.css';
 function App() {
   return (
     <Router>
-      <div>Movie Card Library CRUD</div>
+      <Switch>
+        <div>Movie Card Library CRUD</div>
+        <Route exact path='/' component={MovieList} />
+        <Route exact path='/movies/new' component={NewMovie} />
+        <Route exact path='/movies/:id' component={MovieDetails} />
+        <Route exact path='/movies/:id/edit' component={EditMovie} />
+        <Route exact path='/' component={NotFound} />
+      </Switch>
     </Router>
   );
 }
