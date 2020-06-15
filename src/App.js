f172ce0c3eca1db733fbc40d57866ed97405eec9
Lourenço <loulage@gmail.com>
2020-06-15 18:38:02 -0300

    dekleted linter

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import MovieList from './pages/MovieList';
 
 function App() {
   return (
-      <Router>
-        <div>Movie Card Library CRUD</div>
-        <MovieList />
-      </Router>
+    <Router>
+      <div>Movie Card Library CRUD</div>
+      <MovieList />
+    </Router>
   );
 }
 
