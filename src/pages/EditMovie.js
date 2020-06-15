806cfcc134460701a70e7f193f4b98d5791dbf7a
joaosoutto <joaosoutto@hotmail.com>
2020-06-15 18:54:42 -0300

    10 ja foram, faltam 10

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,26 +1,40 @@
 import React, { Component } from 'react';
-
-import { MovieForm } from '../components';
+import { Redirect } from 'react-router-dom';
+import { MovieForm, Loading } from '../components';
 import * as movieAPI from '../services/movieAPI';
 
 class EditMovie extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      shouldRedirect: null,
+      movie: null,
+      status: 'loading',
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleSubmit(updatedMovie) {
+    movieAPI.updateMovie(updatedMovie)
+    .then((shouldRedirect) => this.setState({shouldRedirect}));
+  }
+
+  componentDidMount() {
+    movieAPI.getMovie(this.props.match.params.id)
+    .then((movie) => this.setState({
+      movie,
+      status: null,
+    }))
   }
 
   render() {
     const { status, shouldRedirect, movie } = this.state;
     if (shouldRedirect) {
-      // Redirect
+      return <Redirect to="/" />
     }
 
     if (status === 'loading') {
-      // render Loading
+      return <Loading />
     }
 
     return (
