fa131d8fd44580211f175f8a7b73392216337529
Fabiola Deiro <fabioladeiro7@gmail.com>
2020-06-15 19:31:31 -0300

    Requisito 6

--- src/App.js
+++ src/App.js
@@ -4,14 +4,18 @@ import './App.css';
 import MovieList from './pages/MovieList';
 import MovieDetails from './pages/MovieDetails';
 import EditMovie from './pages/EditMovie';
+import NewMovie from './pages/NewMovie';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={MovieList} />
+        <Route exact path="/movies/new" component={NewMovie} />
         <Route exact path="/movies/:id" component={MovieDetails} />
         <Route exact path="/movies/:id/edit" component={EditMovie} />
+        <Route path="/" component={NotFound} />
       </Switch>
     </BrowserRouter>
   );
