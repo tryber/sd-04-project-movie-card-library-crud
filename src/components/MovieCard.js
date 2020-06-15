417e9801c6dee95fd2f005c57b5b853140ad8848
Lourenço <loulage@gmail.com>
2020-06-15 17:10:03 -0300

    IMplemented moviecard component -> passed data as props, displayed it and added LINK component, to={id}

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
+    const { id, title, subtitle, storyline, rating, imagePath, bookmarked, genre } = this.props.movie;
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <img src={imagePath} alt={title} />
+        <h2>{title}</h2>
+        <h4>{subtitle}</h4>
+        <p>{storyline}</p>
+        <span>{rating}</span>
+        <span>{genre}</span>
+        <div>
+          <Link to={`/${id}`}>VER DETALHES</Link>
+        </div>
       </div>
     );
   }
