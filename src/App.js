2af9ccd7ec46511b4cc18324c63cac6501dbaef1
Thiago Tavares <thiagooo15@gmail.com>
2020-06-15 15:33:08 -0300

    testando

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Movielist from './pages/MovieList';
+import EditMovie from './pages/EditMovie';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import NotFound from './pages/NotFound';
+
 
 function App() {
   return (
-    <Router>
-      <div>Movie Card Library CRUD</div>
-    </Router>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" component={ Movielist } />
+        <Route path="/movies/new" component={ NewMovie } />
+      </Switch>    
+    </BrowserRouter>
   );
 }
 
