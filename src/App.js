1f53376c9c8f421d03079a947b0a914b7e180c6b
Thiago Tavares <thiagooo15@gmail.com>
2020-06-15 16:14:19 -0300

    finalizando

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import MovieList from './pages/MovieList';
+import NewMovie from './pages/NewMovie';
+import MovieDetails from './pages/MovieDetails';
+import EditMovie from './pages/EditMovie';
+import Error from './pages/error';
 
 function App() {
   return (
     <Router>
-      <div>Movie Card Library CRUD</div>
+      <Link to="/movies/new">ADICIONAR CARTÃO</Link>
+      <Switch>
+        <Route exact path="/movies/new" component={NewMovie} />
+        <Route exact path="/movies/:id" component={MovieDetails} />
+        <Route exact path="/movies/:id/edit" component={EditMovie} />
+        <Route exact path="/" component={MovieList} />
+        <Route component={Error} />
+      </Switch>
     </Router>
   );
 }
---
+++ src/pages/error.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Error extends Component {
+  render() {
+    return <div data-testid="404-error">Error 404</div>;
+  }
+}
+
+export default Error;
