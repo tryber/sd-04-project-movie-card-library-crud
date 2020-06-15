f22be4607a3042192f95c27f3b71aa59b0b605aa
deboracosilveira <deboracosilveira@gmail.com>
2020-06-15 17:11:43 -0300

    requirement-3

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends React.Component {
           <Route exact path="/movies/:id" component={MovieDetails}/>
           <Route exact path="/movies/new" component={NewMovie}/>
           <Route exact path="/movies/:id/edit" component={EditMovie}/>
-          <Route exact path="/" component={NotFound}/>
+          <Route  path="/" component={NotFound}/>
         </Switch>
       </BrowserRouter>
     )
