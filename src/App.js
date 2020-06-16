c5292e82523f7cb89baba52c7bf55811f7b6f587
douglaza <douglaza@gmail.com>
2020-06-15 22:01:17 -0300

    1st requirement - 6th attempt

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
 import MovieList from './pages/MovieList';
 import NewMovie from './pages/NewMovie';
-// import MovieDetails from './pages/MovieDetails';
+import MovieDetails from './pages/MovieDetails';
 import EditMovie from './pages/EditMovie';
 import NotFound from './pages/NotFound';
 
@@ -23,7 +23,7 @@ class App extends Component {
         {/* <main> */}
         <Switch>
           <Route exact path="/" component={MovieList} />
-          {/* <Route exact path="/movies/:id" component={MovieDetails} /> */}
+          <Route exact path="/movies/:id" component={MovieDetails} />
           <Route exact path="/movies/new" component={NewMovie} />
           <Route exact path="/movies/:id/edit" component={EditMovie} />
           <Route component={NotFound} />
