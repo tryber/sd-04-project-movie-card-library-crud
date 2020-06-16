4eec01c468871fc5bc1279318b298b41fe2ed00f
danilossousa <danilosantossousa@gmail.com>
2020-06-16 09:50:58 -0300

    v0.1.0.11

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -50,4 +50,12 @@ class MovieDetails extends Component {
   }
 }
 
+MovieDetails.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.number,
+    }),
+  }).isRequired,
+};
+
 export default MovieDetails;
