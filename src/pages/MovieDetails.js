40877f574412b42a4089c68f1a526f71d07303d5
danilossousa <danilosantossousa@gmail.com>
2020-06-16 09:54:16 -0300

    v0.1.0.12

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import { Loading } from '../components';
 import * as movieAPI from '../services/movieAPI';
 
