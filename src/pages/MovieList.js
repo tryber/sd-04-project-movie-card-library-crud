3229c0a3569c107325971c2c6d955bc1967c3f6e
douglaza <douglaza@gmail.com>
2020-06-15 21:34:19 -0300

    2nd requirement - 1st attempt

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,17 +1,31 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
-
+import Loading from '../components/Loading';
 import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: '',
+    };
+  }
+
+  componentDidMount() {
+    movieAPI.getMovies().then((json) => this.setState({ movie: json }));
+  }
   render() {
     const { movies } = this.state;
 
     // Render Loading here if the request is still happening
-
-    return (
+    return movies === '' ? (
+      <Loading />
+    ) : (
+      // return
       <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
+        {movies.map((movie) => (
+          <MovieCard key={movie.title} movie={movie} />
+        ))}
       </div>
     );
   }
