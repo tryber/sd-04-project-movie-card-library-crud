62cbda4fdb30f2e043861de0f051c431215d4e18
TerezaCarvalho <Familia@MacBook-Air-de-FamiliaCarvalhoAugusto.local>
2020-06-16 15:01:32 -0300

    codeclimate

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
-    const { movie } = this.props
+    const { movie } = this.props;
     const { title, storyline, id } = movie;
     return (
       <div data-testid="movie-card">
