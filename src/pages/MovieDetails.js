d30680e04796a1ec6b57795444ac37ac60c5c466
danilossousa <danilosantossousa@gmail.com>
2020-06-16 05:30:08 -0300

    v0.1.0.4

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -7,6 +7,7 @@ class MovieDetails extends Component {
   render() {
     // Change the condition to check the state
     if (true) return <Loading />;
+    return <p></p>
 
     // const { title, storyline, imagePath, genre, rating, subtitle } = movie;
 
