1be5a9df53859196211d3c88e5f6e19c993550b3
frederico-campello <fredfmc33@gmail.com>
2020-06-16 08:49:07 -0300

    Componentes MovieCard & MovieDetails

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <h4>{movie.title}</h4>
+        <p>{movie.storyline}</p>
+        <Link to={`/movies/${movie.id}`}>VER DETALHES</Link>
       </div>
     );
   }
--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,22 +1,52 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
 
 class MovieDetails extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+      movie: {},
+    }
+    // console.log(this.props);
+  }
+
+  componentDidMount() {
+    //console.log(this.props.match.params);
+
+    let movieId = this.props.match.params.id;
+    movieAPI.getMovie(movieId).then((movie) => {
+      this.setState({
+        loading: false,
+        movie: movie,
+      });
+    });
+  }
+  
   render() {
     // Change the condition to check the state
-    if (true) return <Loading />;
+    const { loading, movie } = this.state;
+    if (loading === true) return <Loading />;
 
-    const { title, storyline, imagePath, genre, rating, subtitle } = movie;
+    const { title, storyline, imagePath, genre, rating, subtitle } = this.state.movie;
 
     return (
       <div data-testid="movie-details">
         <img alt="Movie Cover" src={`../${imagePath}`} />
+        <p>{`Title: ${title}`}</p>
         <p>{`Subtitle: ${subtitle}`}</p>
         <p>{`Storyline: ${storyline}`}</p>
         <p>{`Genre: ${genre}`}</p>
         <p>{`Rating: ${rating}`}</p>
+        <div>
+          <Link to={`/movies/${movie.id}/edit`}>EDITAR</Link>
+        </div>
+        <div>
+          <Link to="/">VOLTAR</Link>
+        </div>
       </div>
     );
   }
