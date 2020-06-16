9ab6736963f43aa7f91deeb19f304845e2947259
Josias Viveiro <josiasviveiro@gmail.com>
2020-06-16 00:03:24 -0300

    Requisito 1 - incompleto

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
+import './pages';
+import { MovieList, NotFound, MovieDetails, NewMovie, EditMovie } from './pages';
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/movies/new' component={NewMovie} />
+        <Route exact path='/movies/:id/edit' component={EditMovie} />
+        <Route exact path='/movies/:id' component={MovieDetails} />
+        <Route exact path='/' component={MovieList} />
+        <Route path='/' component={NotFound} />
+      </Switch>
       <div>Movie Card Library CRUD</div>
-    </Router>
+    </BrowserRouter>
   );
 }
 
