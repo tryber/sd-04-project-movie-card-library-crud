887c8cc2fd0afb74320344bac45bf8531057c378
Julio Cesar <stininmatic@hotmail.com>
2020-06-17 17:04:30 -0300

    Testando EsLint novamente

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,19 +1,27 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-class MovieCard extends React.Component {
-  render() {
-    const { movie } = this.props;
-    return (
-      <div data-testid="movie-card">
-        <img src={movie.imagePath} alt=""/>
-        <p>{movie.title}</p>
-        <p>{movie.storyline}</p>
+const MovieCard = (props) => {
+  const { movie } = props;
+  return (
+    <div data-testid="movie-card">
+      <img src={movie.imagePath} alt="" />
+      <p>{movie.title}</p>
+      <p>{movie.storyline}</p>
 
-        <Link to={`/movies/${movie.id}`}>VER DETALHES</Link>
-      </div>
-    );
-  }
-}
+      <Link to={`/movies/${movie.id}`}>VER DETALHES</Link>
+    </div>
+  );
+};
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    id: PropTypes.number,
+    imagePath: PropTypes.string,
+    storyline: PropTypes.string,
+    title: PropTypes.string,
+  }).isRequired,
+};
 
 export default MovieCard;
