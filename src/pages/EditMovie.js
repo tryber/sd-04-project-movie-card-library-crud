6722f673d64e19c350f6f878d729e515b50736d4
DanielLMiranda <danieldelacerdamiranda@gmail.com>
2020-06-17 23:04:02 -0300

    fix/codeclimate

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -42,4 +42,12 @@ class EditMovie extends Component {
   }
 }
 
+EditMovie.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+  }).isRequired,
+};
+
 export default EditMovie;
