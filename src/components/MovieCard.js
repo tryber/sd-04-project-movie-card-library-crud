a39c624bf482675c3812b9d1a143606bbe144c11
danilossousa <danilosantossousa@gmail.com>
2020-06-18 08:44:05 -0300

    v0.6.0.1

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <img src={movie.imagePath} alt="Filme" />
+        <p>{movie.title}</p>
+        <p>{movie.storyline}</p>
+        <Link to={`movies/${movie.id}`}>VER DETALHES</Link>
       </div>
     );
   }
