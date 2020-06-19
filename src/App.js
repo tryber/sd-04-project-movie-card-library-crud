8636abd27e638ba8f61f5ba59958c2e928214c5d
Leandro Marten <leandromarten1@gmail.com>
2020-06-19 15:07:28 -0300

    Requisito 1 - Completo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
 
 class App extends Component {
   render() {
-    return <BrowserRouter></BrowserRouter>;
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={MovieList} />
+          <Route exact path="/movies/:id" component={MovieDetails} />
+          <Route exact path="/movies/new" component={NewMovie} />
+          <Route exact path="/movies/:id/edit" component={EditMovie} />
+        </Switch>
+      </BrowserRouter>
+    );
   }
 }
 
