076b6ba54f6fbbb1075a5e005a6ca52a03cadb79
Leandro Marten <leandromarten1@gmail.com>
2020-06-19 17:46:43 -0300

    Botao ADD Filme

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import MovieCard from '../components/MovieCard';
 import Loading from '../components/Loading';
 
@@ -28,10 +29,13 @@ class MovieList extends Component {
 
     if (load) {
       return (
-        <div data-testid="movie-list" className="app">
-          {movies.map((movie) => (
-            <MovieCard key={movie.title} movie={movie} />
-          ))}
+        <div>
+          <Link to="/movies/new">ADICIONAR CARTÃO</Link>
+          <div data-testid="movie-list" className="app">
+            {movies.map((movie) => (
+              <MovieCard key={movie.title} movie={movie} />
+            ))}
+          </div>
         </div>
       );
     }
