68bd1179d8827928c48b3c9e36796cef7ce0312f
djonata-dias <diasdjonata@gmail.com>
2020-06-20 04:01:29 -0300

    Criei a página de detalhes do filme que possui a descrição detalhada do filme e um link para voltar para a página principal e outro para editar o filme

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,22 +1,49 @@
 import React, { Component } from 'react';
-
+import { Link } from 'react-router-dom';
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
 
 class MovieDetails extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movie: {
+        title: '',
+        storyline: '',
+        genre: '',
+        rating: 0,
+        subtitle: '',
+        imagePath: '',
+        id: '',
+      },
+      loading: true,
+    };
+  }
+  async componentDidMount() {
+    await movieAPI.getMovie(this.props.match.params.id)
+      .then((movie) => {
+        this.setState({ movie, loading: false });
+      });
+  }
   render() {
-    // Change the condition to check the state
-    if (true) return <Loading />;
-
-    const { title, storyline, imagePath, genre, rating, subtitle } = movie;
-
+    const { movie, loading } = this.state;
+    const { storyline, imagePath, genre, rating, title, subtitle, id } = movie;
+    if (loading) {
+      return <Loading />;
+    }
     return (
-      <div data-testid="movie-details">
-        <img alt="Movie Cover" src={`../${imagePath}`} />
+      <div data-testid="movie-details ">
+        <h1>{title}</h1>
+        <img alt="Movie Cover" src={`/${imagePath}`} />
         <p>{`Subtitle: ${subtitle}`}</p>
         <p>{`Storyline: ${storyline}`}</p>
         <p>{`Genre: ${genre}`}</p>
         <p>{`Rating: ${rating}`}</p>
+        <div>
+          <Link to={'/'}>Voltar</Link>
+          <Link to={`/movies/${id}/edit`} >Editar</Link>
+          <Link to={'/'} onClick={() => movieAPI.deleteMovie(id)} >Deletar</Link>
+        </div>
       </div>
     );
   }
