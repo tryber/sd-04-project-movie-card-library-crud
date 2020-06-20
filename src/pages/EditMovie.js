e367a7cdd0ce86ee77fbc8c598a0af83e32b4976
djonata-dias <diasdjonata@gmail.com>
2020-06-20 04:00:25 -0300

    Criei a página EditMovie que edita o filme com id especifico e redireciona para os detalhes do filme quando salvo as alterações

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,26 +1,40 @@
 import React, { Component } from 'react';
-
-import { MovieForm } from '../components';
+import { Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { MovieForm, Loading } from '../components';
 import * as movieAPI from '../services/movieAPI';
 
 class EditMovie extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      movie: '',
+      status: 'loading',
+      shouldRedirect: '',
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  async componentDidMount() {
+    await movieAPI.getMovie(this.props.match.params.id)
+      .then((movie) => {
+        this.setState({ movie, status: 'true' });
+      });
   }
 
   handleSubmit(updatedMovie) {
+    movieAPI.updateMovie(updatedMovie);
+    const id = this.props.match.params.id;
+    this.setState({ shouldRedirect: `/movies/${id}` });
   }
 
   render() {
     const { status, shouldRedirect, movie } = this.state;
     if (shouldRedirect) {
-      // Redirect
+      return <Redirect to={shouldRedirect} />;
     }
 
     if (status === 'loading') {
-      // render Loading
+      return <Loading />;
     }
 
     return (
@@ -30,5 +44,12 @@ class EditMovie extends Component {
     );
   }
 }
+
+EditMovie.PropTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({ id: PropTypes.number.isRequired }),
+  }),
+};
+
 
 export default EditMovie;
