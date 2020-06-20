ec37c9a230025f588de840e70eddb2bcee2af2ab
djonata-dias <diasdjonata@gmail.com>
2020-06-20 03:54:10 -0300

    Adicionei as rotas para as páginas no App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { NewMovie, MovieDetails, EditMovie, MovieList, NotFound } from './pages';
 
 function App() {
   return (
-      <div>Movie Card Library CRUD</div>
+    <Router>
+      <div>
+        <header>
+          <h1>Movie Card Library CRUD</h1>
+          <Link to="/movies/new">Criar Novo Filme</Link>
+        </header>
+        <main>
+          <Switch>
+            <Route exact path="/" component={MovieList} />
+            <Route exact path="/movies/new" component={NewMovie} />
+            <Route exact path="/movies/:id" component={MovieDetails} />
+            <Route exact path="/movies/:id/edit" component={EditMovie} />
+            <Route path="/" component={NotFound} />
+          </Switch>
+        </main>
+      </div>
+    </Router>
+
   );
 }
 
