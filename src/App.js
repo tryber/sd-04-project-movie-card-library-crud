30175d11f737199c1bbab3da8501fd4afaf61bf9
JorgeHSOsiro <jojimurakabe@gmail.com>
2020-06-21 16:33:42 -0300

    showing MovieDetails

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 import MovieList from './pages/MovieList';
 import MovieDetails from './pages/MovieDetails';
 import NewMovie from './pages/NewMovie';
 import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
 import './App.css';
 
 
@@ -13,9 +14,10 @@ function App() {
         <div className="page-title">Movie Card Library CRUD</div>
         <Switch>
           <Route exact path="/" component={MovieList} />
-          <Route path="/movies/:id" component={MovieDetails} />
-          <Route path="/movies/new" component={NewMovie} />
-          <Route path="/movies/:id/edit" component={EditMovie} />
+          <Route exact path="/movies/:id" component={MovieDetails} />
+          <Route exact path="/movies/new" component={NewMovie} />
+          <Route exact path="/movies/:id/edit" component={EditMovie} />
+          <Route component={NotFound} />
         </Switch>
       </BrowserRouter>
   );
--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,8 +1,8 @@
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
-
+import { Link } from 'react-router-dom';
 import * as movieAPI from '../services/movieAPI';
-// import { Loading } from '../components';
+import { Loading } from '../components';
 
 class MovieDetails extends Component {
   constructor(props) {
@@ -13,34 +13,40 @@ class MovieDetails extends Component {
   }
 
   componentDidMount() {
-    movieAPI.getMovie()
-    .then(() => {
+    movieAPI.getMovie(this.props.match.params.id)
+    .then((movie) => {
       this.setState({
         isLoaded: true,
+        movie,
       });
     });
   }
   render() {
     // Change the condition to check the state
-    // const { isLoaded, movie } = this.props;
-    // if (!isLoaded) return <Loading />;
-    const { title, storyline, imagePath, genre, rating, subtitle } = this.state;
+    const { isLoaded, movie } = this.state;
+    if (!isLoaded) { 
+      return <Loading />;
+    }
+    const { title, storyline, imagePath, genre, rating, subtitle } = movie;
 
     return (
       <div data-testid="movie-details">
         <img alt="Movie Cover" src={`../${imagePath}`} />
+        <p>{`Title: ${title}`}</p>
         <p>{`Subtitle: ${subtitle}`}</p>
         <p>{`Storyline: ${storyline}`}</p>
         <p>{`Genre: ${genre}`}</p>
         <p>{`Rating: ${rating}`}</p>
+        <Link to="/movies/:id/edit">EDITAR</Link>
+        <Link to="/">VOLTAR</Link>
       </div>
     );
   }
 }
 
-// MovieDetails.propTypes = {
-//   isLoaded: PropTypes.bool.isRequired,
-//   movie: PropTypes.shape.isRequired,
-// };
+MovieDetails.propTypes = {
+  isLoaded: PropTypes.bool.isRequired,
+  movie: PropTypes.shape.isRequired,
+};
 
 export default MovieDetails;
