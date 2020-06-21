1b707edb3d38e646b0a8b0a00e5e310e68ce8a58
Kaecio <kaecio@yahoo.com.br>
2020-06-21 20:40:58 -0300

    testando requisito 12

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 
 const MovieCard = (props) => {
   const { movie } = props;
+  console.log(props)
   return (
     <div data-testid="movie-card">
       <img src={movie.imagePath} alt="movie Card" />
