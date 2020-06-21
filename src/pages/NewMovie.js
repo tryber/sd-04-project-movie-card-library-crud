aa421e46b342a7797df1ccbaa3f817b76d354abf
André Ferreira <andrefpires1@gmail.com>
2020-06-21 20:36:01 -0300

    componente NewMovie

--- src/pages/NewMovie.js
+++ src/pages/NewMovie.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
-
 import MovieForm from '../components/MovieForm';
+import MovieForm from 'react-router-dom';
 import * as movieAPI from '../services/movieAPI';
 
 class NewMovie extends Component {
   constructor(props) {
     super(props);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.state = ({ redirect: false});
   }
 
   handleSubmit(newMovie) {
+    movieAPI.createMovie(newMovie);
+    this.setState = ({ redirect:true });
   }
 
   render() {
+    if (this.state.shouldRedirect) return <Redirect to="/" />;
     return (
       <div data-testid="new-movie">
         <MovieForm onSubmit={this.handleSubmit} />
