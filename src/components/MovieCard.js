92fc6b561ef04c8f454044449cc6481f76fa557c
Kaecio <kaecio@yahoo.com.br>
2020-06-21 15:49:45 -0300

    Alinhado componentes

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -5,8 +5,13 @@ class MovieCard extends React.Component {
     const {movie} =this.props;
     const {title, subtitle, storyline, imagePath} = movie;
     return (
-      <div data-testid="movie-card">
-        Movie Card
+      <div className="movie-card">
+        <img className="movie-card-img" src-{imagePath} alt="movie Card" />
+        <div className="movie-screen">
+        <h4 className="movie-title">{title}</h4>
+        <h5 className="movie-subtitle">{subtitle}</h5>
+        <p className="movie-storyline">{storyline}</p>
+        </div>
       </div>
     );
   }
