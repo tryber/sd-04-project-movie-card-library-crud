852ef11d7ad13f43fd6c3d3039720217c08e365d
André Ferreira <andrefpires1@gmail.com>
2020-06-21 18:06:55 -0300

    Adicionando rotas App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { NotFound, MovieList, NewMovie, EditMovie, MovieDetails } from './pages';
 
 function App() {
   return (
-      <div>Movie Card Library CRUD</div>
+    <Router>
+      <div>
+        <header>
+          <h1>Movie Card Library CRUD</h1>
+        </header>
+        <main>
+          <Route component={NotFound} />
+          <Route path="/" component={MovieList} />
+          <Route exact path="/movies/new" component={NewMovie} />
+          <Route exact path="/movies/:id/edit" component={EditMovie} />
+          <Route exact path="/movies/:id" component={MovieDetails} />
+        </main>
+      </div>
+    </Router>
   );
 }
 
