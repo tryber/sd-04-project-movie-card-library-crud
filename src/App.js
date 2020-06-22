d95241a178f00a5de94540cc364905e0206b4edd
Hugo J. De Moro Jr <hugo.urano@gmail.com>
2020-06-22 12:24:35 -0300

    comenting not defined code

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ function App() {
   return (
     <BrowserRouter>
     <Switch>
-      <Route exact path="/" component={MovieList} />
+      {/* <Route exact path="/" component={MovieList} />
       <Route exact path="/movies/new" component={NewMovie} />
       <Route exact path="/movies/:id" component={MovieDetails} />
       <Route exact path="/movies/:id/edit" component={EditMovie} />
-      <Route path="/" component={NotFound} />
+      <Route path="/" component={NotFound} /> */}
     </Switch>
   </BrowserRouter>
   );
