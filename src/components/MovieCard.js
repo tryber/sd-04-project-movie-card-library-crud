afdf2a212a8fbce06caf67a1d64b4f97b38d5832
Josias Viveiro <josiasviveiro@gmail.com>
2020-06-22 02:08:46 -0300

    MovieCard

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
     return (
-      <div data-testid="movie-card">
-        Movie Card
+      <div data-testid="movie-card" className="movie-card">
+        <img alt="Movie Cover" className="movie-card-image" src={movie.imagePath} />
+        <div className="movie-card-body">
+          <h4 className="movie-card-title">{movie.title}</h4>
+          <p className="movie-card-storyline">{movie.storyline}</p>
+          <Link to={`/movies/${movie.id}`}>VER DETALHES</Link>
+        </div>
       </div>
     );
   }
