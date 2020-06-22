224b41341fbc07d0d4fb8a6763071d627e66854e
André Ferreira <andrefpires1@gmail.com>
2020-06-21 22:41:23 -0300

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import { NotFound, MovieList, NewMovie, EditMovie, MovieDetails } from './pages';
 
 function App() {
@@ -8,13 +8,16 @@ function App() {
       <div>
         <header>
           <h1>Movie Card Library CRUD</h1>
+          <Link to="/movies/new">Adicionar Cartão</Link>
         </header>
         <main>
-          <Route component={NotFound} />
-          <Route path="/" component={MovieList} />
-          <Route exact path="/movies/new" component={NewMovie} />
-          <Route exact path="/movies/:id/edit" component={EditMovie} />
-          <Route exact path="/movies/:id" component={MovieDetails} />
+          <Switch>
+            <Route component={NotFound} />
+            <Route path="/" component={MovieList} />
+            <Route exact path="/movies/new" component={NewMovie} />
+            <Route exact path="/movies/:id/edit" component={EditMovie} />
+            <Route exact path="/movies/:id" component={MovieDetails} />
+          </Switch>
         </main>
       </div>
     </Router>
