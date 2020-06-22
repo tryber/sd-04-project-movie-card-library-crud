3e377347e7dd7aeea0a976626d3ab8c92f792216
Hugo J. De Moro Jr <hugo.urano@gmail.com>
2020-06-22 13:32:45 -0300

    component working

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React from 'react';
-import { BrowserRouter, /* Route, */ Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import MovieList from './component/MovieList';
+import EditMovie from './component/EditMovie';
+import MovieDetails from './component/MovieDetails';
+import NewMovie from './component/NewMovie';
+import NotFound from './component/NotFound';
 
 function App() {
   return (
     <BrowserRouter>
-    <Switch>
-      {/* <Route exact path="/" component={MovieList} />
-      <Route exact path="/movies/new" component={NewMovie} />
-      <Route exact path="/movies/:id" component={MovieDetails} />
-      <Route exact path="/movies/:id/edit" component={EditMovie} />
-      <Route path="/" component={NotFound} /> */}
-    </Switch>
-  </BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={MovieList} />
+        <Route exact path="/movies/new" component={NewMovie} />
+        <Route exact path="/movies/:id" component={MovieDetails} />
+        <Route exact path="/movies/:id/edit" component={EditMovie} />
+        <Route path="/" component={NotFound} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
