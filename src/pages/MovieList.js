3f9057940295080a4bec6595eaa6f6267117504f
Josias Viveiro <josiasviveiro@gmail.com>
2020-06-22 01:31:41 -0300

    MovieList

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
-
+import Loading from '../components/Loading';
 import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: '',
+    };
+  }
+
+  componentDidMount() {
+    movieAPI.getMovies().then((movies) => this.setState({ movies }));
+  }
+
   render() {
     const { movies } = this.state;
 
     // Render Loading here if the request is still happening
+    if (!movies) return <Loading />
 
     return (
       <div data-testid="movie-list">
