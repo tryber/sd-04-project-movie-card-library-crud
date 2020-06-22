cb08eb467e4e2c45f21f96c93538d5af8fafdb13
JorgeOsiro <jojimurakabe@gmail.com>
2020-06-22 11:39:08 -0300

    req 1 fixed

--- src/App.js
+++ src/App.js
@@ -17,9 +17,9 @@ function App() {
           </header>
           <main>
             <Switch>
-              <Route exact path="/" component={MovieList} />
-              <Route exact path="/movies/:id" component={MovieDetails} />
               <Route exact path="/movies/new" component={NewMovie} />
+              <Route exact path="/movies/:id" component={MovieDetails} />
+              <Route exact path="/" component={MovieList} />
               <Route path="/movies/:id/edit" component={EditMovie} />
               <Route component={NotFound} />
             </Switch>
