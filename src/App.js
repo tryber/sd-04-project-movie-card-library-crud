d782a9ea4df0400e5ea07f750a6764e85bf50b86
gabriellukke <gabriellukke@outlook.com>
2020-06-22 10:20:20 -0300

    Add the routers in App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { EditMovie, MovieDetails, MovieList, NewMovie, NotFound } from './pages';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
 
-function App() {
-  return (
-      <div>Movie Card Library CRUD</div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+        <div>
+          <h1>Movie Card Library CRUD</h1>
+          <Router>
+            <Switch>
+              <Route exact path="/" component={MovieList} />
+              <Route exact path="/movies/new" component={NewMovie} />
+              <Route exact path="/movies/:id" component={MovieDetails} />
+              <Route exact path="/movies/:id/edit" component={EditMovie} />
+              <Route component={NotFound} />
+            </Switch>
+          </Router>
+        </div>
+    );
+  }
 }
 
 export default App;
