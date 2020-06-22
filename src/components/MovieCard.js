c14b864c721adcf021c41c7dbc00253454cc83db
Josias Viveiro <josiasviveiro@gmail.com>
2020-06-22 02:20:29 -0300

    MovieCard props validation

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,20 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
-    const { movie } = this.props;
+    const { id, title, storyline, imagePath } = this.props.movie;
+
     return (
       <div data-testid="movie-card" className="movie-card">
-        <img alt="Movie Cover" className="movie-card-image" src={movie.imagePath} />
+        <img alt="Movie Cover" className="movie-card-image" src={imagePath} />
         <div className="movie-card-body">
-          <h4 className="movie-card-title">{movie.title}</h4>
-          <p className="movie-card-storyline">{movie.storyline}</p>
-          <Link to={`/movies/${movie.id}`}>VER DETALHES</Link>
+          <h4 className="movie-card-title">{title}</h4>
+          <p className="movie-card-storyline">{storyline}</p>
+          <Link to={`/movies/${id}`}>VER DETALHES</Link>
         </div>
       </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    id: PropTypes.number,
+    title: PropTypes.string,
+    storyline: PropTypes.string,
+    imagePath: PropTypes.string,
+  }).isRequired,
+};
 
 export default MovieCard;
