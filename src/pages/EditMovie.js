62ac2fd72d5f267c9ceaf84654d5b85cdf947094
gabriellukke <gabriellukke@outlook.com>
2020-06-22 12:49:49 -0300

    Add Loading Screen to EditMovie component

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,26 +1,32 @@
 import React, { Component } from 'react';
-
-import { MovieForm } from '../components';
+import { Redirect } from 'react-router-dom';
+import { MovieForm, Loading } from '../components';
 import * as movieAPI from '../services/movieAPI';
 
 class EditMovie extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      movie: [],
+      loading: false,
+      shouldRedirect: false,
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleSubmit(updatedMovie) {
-  }
+    movieAPI.updateMovie(updatedMovie)
+      .then(this.setState({ shouldRedirect: true, }))
+  };
 
   render() {
-    const { status, shouldRedirect, movie } = this.state;
+    const { loading, shouldRedirect, movie } = this.state;
     if (shouldRedirect) {
-      // Redirect
+      return <Redirect to="/" />;
     }
 
-    if (status === 'loading') {
-      // render Loading
+    if (!loading) {
+      return <Loading />;
     }
 
     return (
