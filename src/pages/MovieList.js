65103a0af54279aee3743cfd3e70035cd1bbc517
Josias Viveiro <josiasviveiro@gmail.com>
2020-06-22 01:34:31 -0300

    line 22 - ;

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -19,7 +19,7 @@ class MovieList extends Component {
     const { movies } = this.state;
 
     // Render Loading here if the request is still happening
-    if (!movies) return <Loading />
+    if (!movies) return <Loading />;
 
     return (
       <div data-testid="movie-list">
