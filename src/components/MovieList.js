3a57bb603cae8378766bf4d3deb7540980cb5e3f
Hugo J. De Moro Jr <hugo.urano@gmail.com>
2020-06-24 10:54:35 -0300

    working on MovieList component

---
+++ src/components/MovieList.js
@@ -0,0 +1,19 @@
+
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import MovieCard from '../components/MovieCard';
+import Loading from '../components/Loading';
+
+import * as movieAPI from '../services/movieAPI';
+
+class MovieList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { movies: [], isLoaded: false };
+  }
+
+
+}
+
+export default MovieList;
