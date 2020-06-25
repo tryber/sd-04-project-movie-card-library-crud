0a6676201e943dd4e5ecc0e1b0c90ef866526b1b
wistyk <leonardoxw@gmail.com>
2020-06-25 19:06:01 -0300

    req2

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import MovieList from './pages/MovieList';
 import MovieDetails from './pages/MovieDetails';
 import NewMovie from './pages/NewMovie';
 import EditMovie from './pages/EditMovie';
-import NotFound from '../../sd-04-project-frontend-online-store-02/src/components/MainContent/Product List-Card/NotFound';
+import NotFound from './pages/NotFound';
+import './App.css'
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={MovieList} />
-        <Route exact path="/movies/:id" component={MovieDetails} />
         <Route exact path="/movies/new" component={NewMovie} />
+        <Route exact path="/movies/:id" component={MovieDetails} />
         <Route exact path="/movies/:id/edit" component={EditMovie} />
         <Route component={NotFound} />
       </Switch>
