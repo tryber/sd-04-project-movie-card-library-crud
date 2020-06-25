43dbcd78fa0d82ed8da5bd8024a1f019851dcec7
wistyk <leonardoxw@gmail.com>
2020-06-25 19:10:31 -0300

    CC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import MovieList from './pages/MovieList';
 import MovieDetails from './pages/MovieDetails';
 import NewMovie from './pages/NewMovie';
 import EditMovie from './pages/EditMovie';
 import NotFound from './pages/NotFound';
-import './App.css'
+import './App.css';
 
 function App() {
   return (
--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import MovieCard from '../components/MovieCard';
 import Loading from '../components/Loading';
 import * as movieAPI from '../services/movieAPI';
-import { Link } from 'react-router-dom';
 
 class MovieList extends Component {
   constructor(props) {
     super(props);
-    this.state = { movies: "" };
+    this.state = { movies: '' };
   }
 
   componentDidMount() {
@@ -19,11 +19,11 @@ class MovieList extends Component {
     const { movies } = this.state;
 
     // Render Loading here if the request is still happening
-    if (movies === "") return <Loading />;
+    if (movies === '') return <Loading />;
 
     return (
       <div data-testid="movie-list">
-        <Link to={"/movies/new"}>ADICIONAR CARTÃO</Link>
+        <Link to={'/movies/new'}>ADICIONAR CARTÃO</Link>
         {movies.map((movie) => (
           <MovieCard key={movie.title} movie={movie} />
         ))}
