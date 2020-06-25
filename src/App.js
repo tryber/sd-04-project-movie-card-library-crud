48eaebe9b24a4be2dec613b3a802f9e576893f7f
wistyk <leonardoxw@gmail.com>
2020-06-25 11:07:39 -0300

    hello, hello, hello

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
+    <BrowserRouter>
       <div>Movie Card Library CRUD</div>
+    </BrowserRouter>
   );
 }
 
