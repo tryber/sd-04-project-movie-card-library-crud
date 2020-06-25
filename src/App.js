0a14ca4a4957dbdbe2562881707f7262c3c13c7f
wistyk <leonardoxw@gmail.com>
2020-06-25 12:53:33 -0300

    requisito 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
+import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails'
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
+import NotFound from '../../sd-04-project-frontend-online-store-02/src/components/MainContent/Product List-Card/NotFound';
 
 function App() {
   return (
     <BrowserRouter>
-      <div>Movie Card Library CRUD</div>
+      <Switch>
+        <Route exact path="/" component={MovieList} />
+        <Route exact path="/movies/:id" component={MovieDetails} />
+        <Route exact path="/movies/new" component={NewMovie} />
+        <Route exact path="/movies/:id/edit" component={EditMovie} />
+        <Route component={NotFound} />
+      </Switch>
     </BrowserRouter>
   );
 }
