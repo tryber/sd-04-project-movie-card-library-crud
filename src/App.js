f07ccefcae92be321f9ce16abf3b8f35d8611796
wistyk <leonardoxw@gmail.com>
2020-06-25 11:51:22 -0300

    CC

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from "react";
-import { BrowserRouter } from "react-router-dom";
-import "./App.css";
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import './App.css';
 
 function App() {
   return (
