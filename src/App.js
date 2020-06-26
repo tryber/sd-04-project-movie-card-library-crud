ffbb84760f918882e43a15be9333c85b4c9eda89
yoshidalucas92 <lucaslimayoshida@gmail.com>
2020-06-25 23:49:43 -0300

    acho que só falta func edit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route, Link} from 'react-router-dom';
 import './App.css';
+import { MovieDetails, NewMovie, EditMovie, NotFound, MovieList } from './pages';
 
 function App() {
   return (
-      <div>Movie Card Library CRUD</div>
+      <BrowserRouter>
+        <header>
+          {/* <Link to="/index">Filmes</Link> */}
+          {/* <Link to="/movies/:id">movies</Link> */}
+          <Link to="/movies/new">ADICIONAR CARTÃO</Link>
+          {/* <Link to="/movies/:id/edit">EDIT MOVIE</Link> */}
+        </header>
+        <Switch>
+          <Route exact path="/" component={MovieList} />
+          <Route exact path="/index" component={MovieList} />
+          <Route path="/movies/new" component={NewMovie} />
+          <Route path="/movies/:id" component={MovieDetails} />
+          <Route path="/movies/:id/edit" component={EditMovie} />
+          <Route component={NotFound} />
+        </Switch>
+      </BrowserRouter>
   );
 }
 
