d57b3c10cac1f35f8a6200f83cb79d1216034a7b
PedroParreiras <44480402+PedroParreiras@users.noreply.github.com>
2020-06-27 16:48:38 -0300

    End?

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
+import Loading from '../components/Loading';
 
 import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { movies: '' };
+  }
+  componentDidMount() {
+    movieAPI.getMovies().then((movies) => this.setState({ movies }));
+  }
   render() {
     const { movies } = this.state;
 
-    // Render Loading here if the request is still happening
-
-    return (
+    return (movies === '') ?
+      <Loading /> :
+    (
       <div data-testid="movie-list">
         {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
       </div>
--- src/pages/NewMovie.js
+++ src/pages/NewMovie.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 
 import MovieForm from '../components/MovieForm';
 import * as movieAPI from '../services/movieAPI';
@@ -6,13 +7,17 @@ import * as movieAPI from '../services/movieAPI';
 class NewMovie extends Component {
   constructor(props) {
     super(props);
+    this.state = { status: '' };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleSubmit(newMovie) {
+    movieAPI.createMovie(newMovie)
+    .then((status) => this.setState({ status }));
   }
 
   render() {
+    if (this.state.status === 'OK') return <Redirect to="/" />;
     return (
       <div data-testid="new-movie">
         <MovieForm onSubmit={this.handleSubmit} />
@@ -20,4 +25,5 @@ class NewMovie extends Component {
     );
   }
 }
+
 export default NewMovie;
