3375b43be725e795771f315d6036f7fbc8fea1f1
PedroParreiras <44480402+PedroParreiras@users.noreply.github.com>
2020-06-27 16:31:09 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import './App.css';
+import { MovieList, MovieDetails, EditMovie, NewMovie, NotFound } from './pages';
 
 function App() {
   return (
