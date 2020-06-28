9b593b5ca3714016d33efb5948d208ef1038ff6a
wistyk <leonardoxw@gmail.com>
2020-06-28 11:46:05 -0300

    CC

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { MovieForm, Loading } from '../components';
 import * as movieAPI from '../services/movieAPI';
-import { Redirect } from 'react-router-dom';
 
 class EditMovie extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      movie: "",
+      movie: '',
       status: true,
       shouldRedirect: false,
     };
