21c5d08b60a84da4c7767292d5ef31bd8d1d601b
Rafael Passos Barbosa <59584187+rafaelpbar@users.noreply.github.com>
2020-07-01 15:04:45 -0300

    Teste

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Router } from 'react-router-dom';
 
 function App() {
   return (
+    <Router>
       <div>Movie Card Library CRUD</div>
+    </Router>
   );
 }
 
 export default App;
+
+//lkndslknddslknvfsnlksdnvlksn
