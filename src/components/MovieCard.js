a710dc7d2f675dfea98a3f052361c30268970e4e
fogo5000 <lucaslucas2008@gmail.com>
2020-07-02 23:12:55 -0300

    Do 50% of MovieCard

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
+    const { title, subtitle, storyline, imagePath } = this.props.movie;
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <Link to={`/movies/${title}`} >
+          <div>
+            <img src={imagePath} alt="Capa do Filme" />
+            <div>
+              <h4>{title}</h4>
+              <h5>{subtitle}</h5>
+              <p>{storyline}</p>
+            </div>
+          </div>
+        </Link>
       </div>
     );
   }
