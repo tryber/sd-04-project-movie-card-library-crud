b0131a4f6d3724c8026cf93fa407f8ce0f208139
saturnian99 <saturnian.dsg@gmail.com>
2020-07-05 18:52:05 -0300

    Render loading screen while fetching movies

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -2,18 +2,31 @@ import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
 
 import * as movieAPI from '../services/movieAPI';
+import { Loading } from '../components';
 
 class MovieList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: [],
+    };
+  }
+
+  componentDidMount() {
+    movieAPI.getMovies()
+      .then((movies) => this.setState({ movies }));
+  }
+
   render() {
     const { movies } = this.state;
 
     // Render Loading here if the request is still happening
 
-    return (
+    return movies === [] ? (
       <div data-testid="movie-list">
         {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
       </div>
-    );
+    ) : (<Loading />);
   }
 }
 
