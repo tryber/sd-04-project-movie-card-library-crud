b0c256efe3ea91f3f9d1d98c61c02261b14acda3
fogo5000 <lucaslucas2008@gmail.com>
2020-07-05 21:06:46 -0300

    Do 50% of MovieCard Fix CC 1.0v

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -3,16 +3,16 @@ import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
-    const { title, subtitle, storyline, imagePath } = this.props.movie;
+    //  const { title, subtitle, storyline, imagePath } = this.props.movie;
     return (
       <div data-testid="movie-card">
-        <Link to={`/movies/${title}`} >
+        <Link to={`/movies/${this.props.movie.title}`} >
           <div>
-            <img src={imagePath} alt="Capa do Filme" />
+            <img src={this.props.movie.imagePath} alt="Capa do Filme" />
             <div>
-              <h4>{title}</h4>
-              <h5>{subtitle}</h5>
-              <p>{storyline}</p>
+              <h4>{this.props.movie.title}</h4>
+              <h5>{this.props.movie.subtitle}</h5>
+              <p>{this.props.movie.storyline}</p>
             </div>
           </div>
         </Link>
