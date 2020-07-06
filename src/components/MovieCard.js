3ca7789b2eff7cd0b9fd7aef69963da42d8ed27f
fogo5000 <lucaslucas2008@gmail.com>
2020-07-05 22:11:11 -0300

    Do 50% of MovieCard Fix CC 1.1v

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,24 +1,34 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
-    //  const { title, subtitle, storyline, imagePath } = this.props.movie;
+    const { title, subtitle, storyline, imagePath } = this.props.movie;
     return (
       <div data-testid="movie-card">
-        <Link to={`/movies/${this.props.movie.title}`} >
+        <Link to={`/movies/${title}`} >
           <div>
-            <img src={this.props.movie.imagePath} alt="Capa do Filme" />
+            <img src={imagePath} alt="Capa do Filme" />
             <div>
-              <h4>{this.props.movie.title}</h4>
-              <h5>{this.props.movie.subtitle}</h5>
-              <p>{this.props.movie.storyline}</p>
+              <h4>{title}</h4>
+              <h5>{subtitle}</h5>
+              <p>{storyline}</p>
             </div>
           </div>
         </Link>
       </div>
     );
   }
+}
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    imagePath: PropTypes.string.isRequired
+  })
 }
 
 export default MovieCard;
