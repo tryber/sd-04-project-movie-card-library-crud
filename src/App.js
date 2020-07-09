6c2623b122aa3b57d719d548f11c2ebb0bd319c3
Hugo J. De Moro Jr <hugo.urano@gmail.com>
2020-07-09 18:19:13 -0300

    changing app from function to class

--- src/App.js
+++ src/App.js
@@ -6,21 +6,23 @@ import {
 } from 'react-router-dom';
 import { EditMovie, MovieDetails, MovieList, NewMovie, NotFound } from './pages';
 
-function App() {
+class App extends React.Component {
+  render(){
   return (
       <div>
       <h1>Movie Card Library CRUD</h1>
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={MovieList} />
-          <Route exact path="/movies/new" component={NewMovie} />
-          <Route exact path="/movies/:id" component={MovieDetails} />
-          <Route exact path="/movies/:id/edit" component={EditMovie} />
-          <Route component={NotFound} />
-        </Switch>
-      </BrowserRouter>
-    </div>
-  );
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={MovieList} />
+            <Route exact path="/movies/new" component={NewMovie} />
+            <Route exact path="/movies/:id" component={MovieDetails} />
+            <Route exact path="/movies/:id/edit" component={EditMovie} />
+            <Route component={NotFound} />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
