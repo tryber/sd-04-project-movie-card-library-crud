fcc4652160e311286a2e8a3b2f8fb63ed1c1e273
Hugo J. De Moro Jr <hugo.urano@gmail.com>
2020-07-10 11:40:49 -0300

    removed missing space

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { getMovie  as getMovieDetails } from '../services/movieAPI';
+import { getMovie as getMovieDetails } from '../services/movieAPI';
 import { Loading } from '../components';
 
 class MovieDetails extends Component {
