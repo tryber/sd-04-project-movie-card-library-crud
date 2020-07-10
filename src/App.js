93fc6c28fcf5f55205e77093d90d79cb3f8a4194
Hugo J. De Moro Jr <hugo.urano@gmail.com>
2020-07-09 22:35:51 -0300

    worked on route paths, removed divs

--- src/App.js
+++ src/App.js
@@ -8,18 +8,15 @@ import { EditMovie, MovieDetails, MovieList, NewMovie, NotFound } from './pages'
 
 function App() {
   return (
-    <div>
-    <h1>Movie Card Library CRUD</h1>
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={MovieList} />
-          <Route exact path="/movies/new" component={NewMovie} />
-          <Route exact path="/movies/:id" component={MovieDetails} />
-          <Route exact path="/movies/:id/edit" component={EditMovie} />
-          <Route component={NotFound} />
-        </Switch>
-      </BrowserRouter>
-    </div>
+<BrowserRouter>
+  <Switch>
+    <Route exact path="/" component={MovieList} />
+    <Route path="/movies/new" component={NewMovie} />
+    <Route path="/movies/:id" component={MovieDetails} />
+    <Route path="/movies/:id/edit" component={EditMovie} />
+    <Route path="/" component={NotFound} />
+  </Switch>
+</BrowserRouter>
   );
 }
 
