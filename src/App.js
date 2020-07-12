6a8452613de29ec8750b60e364612ef70d3c3b1a
Lucas Ribas <lucas@mundoribas.com.br>
2020-07-12 19:55:35 -0300

    Requisito 1 - ok

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { MovieList, MovieDetails, NewMovie, EditMovie, NotFound } from './pages/index'
 
 function App() {
-  return (
-      <div>Movie Card Library CRUD</div>
-  );
+  render()
+  {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={MovieList} />
+          <Route path="/movies/new" component={NewMovie} />
+          <Route exact path="/movies/:id" component={MovieDetails} />
+          <Route path="/movies/:id/edit" component={EditMovie} />
+          <Route path="/" component={NotFound} />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
