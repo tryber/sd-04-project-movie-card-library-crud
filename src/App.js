6f8db58c38a9f8a1a90a33b09dd4256506bfd6af
leonardoinatel <leonardodamasceno@gec.inatel.br>
2020-07-13 17:52:54 -0300

    Requisito 1, rotas setadas

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
+import { EditMovie, MovieList, MovieDetails, NewMovie, NotFound} from './pages/index';
+
 function App() {
   return (
-      <div>Movie Card Library CRUD</div>
+    <div>
+    	<div>Movie Card Library CRUD</div>
+    	<BrowserRouter>
+    	  <Switch>
+    	    <Route path="/movies/:id/edit" component={EditMovie} />
+    	    <Route path="/movies/:id" component={MovieDetails} />
+    	    <Route path="/movies/new" component={NewMovie} />
+          <Route exact path="/" component={MovieList} />
+          <Route component={NotFound} />
+    	  </Switch>
+    	</BrowserRouter>
+    </div>
   );
 }
 
