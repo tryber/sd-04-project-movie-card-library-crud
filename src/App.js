cc2a7a71bc7a3da73a7f11a153ee12c5efb2141c
leonardoinatel <leonardodamasceno@gec.inatel.br>
2020-07-13 17:58:47 -0300

    Requisito 1 CC

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-import { EditMovie, MovieList, MovieDetails, NewMovie, NotFound} from './pages/index';
+import {
+  EditMovie,
+  MovieList,
+  MovieDetails,
+  NewMovie,
+  NotFound,
+} from './pages/index';
 
 function App() {
   return (
     <div>
-    	<div>Movie Card Library CRUD</div>
-    	<BrowserRouter>
-    	  <Switch>
-    	    <Route path="/movies/:id/edit" component={EditMovie} />
-    	    <Route path="/movies/:id" component={MovieDetails} />
-    	    <Route path="/movies/new" component={NewMovie} />
+      <div>Movie Card Library CRUD</div>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/movies/:id/edit" component={EditMovie} />
+          <Route path="/movies/:id" component={MovieDetails} />
+          <Route path="/movies/new" component={NewMovie} />
           <Route exact path="/" component={MovieList} />
           <Route component={NotFound} />
-    	  </Switch>
-    	</BrowserRouter>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
