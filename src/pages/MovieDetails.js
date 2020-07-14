12036857fc772fd470da7d26d8ae6841add310f0
leonardoinatel <leonardodamasceno@gec.inatel.br>
2020-07-14 16:02:24 -0300

    Issues

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -43,4 +43,12 @@ class MovieDetails extends Component {
   }
 }
 
+MovieDetails.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+};
+
 export default MovieDetails;
